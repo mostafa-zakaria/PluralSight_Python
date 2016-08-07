0b867c215765d6483a08a6afe4147525ba21eec2
MZX <mostafa.zakaria@gmail.com>
2016-08-07 14:57:31 +0100

    First Stage of Class Building - Cart

--- Shoping_Cart.py
+++ Shoping_Cart.py
@@ -1,3 +1,29 @@
+class Cart:
+    def __init__(self):
+        self._contents = dict()
+
+    def add_item(self, item):
+        if not item in self._contents:
+            self._contents[item] = 0
+        self._contents[item] += 1
+
+    def remove_item(self, item):
+        if item in self._contents:
+            if self._contents[item] == 1:
+                del self._contents[item]
+            else:
+                self._contents[item] -= 1
+
+    def contains(self, item):
+        flag = False
+        if (item in self._contents):
+            flag = True
+        return flag
+
+    def print_cart(self):
+        print(self._contents)
+        
+        
 def get_order():
     print("[command] [item] (Command is A to Add, D to Delete, Q to Quit, P to Print)")
     line = input()
@@ -5,44 +31,28 @@ def get_order():
     command = line[:1]
     item = line[2:]
     return (command, item)
-    
-def add_cart(item, cart):
-    if not item in cart:
-        cart[item] = 0
-    cart[item] += 1
-
-def remove_cart(item, cart):
-    if item in cart:
-        if cart[item] == 1:
-            del cart[item]
-        else:
-            cart[item] -= 1
-
+      
 def process_order(order, cart):
     flag = True
     command, item = order
 
     if (command == 'A'):
-        add_cart(item, cart)
-    if (command == 'D') and (item in cart):
-        remove_cart(item, cart)
+        cart.add_item(item)
+    if (command == 'D') and (cart.contains(item)):
+        cart.remove_item(item)
     if command == 'P':
-        print(cart)
+        cart.print_cart()
     if command == 'Q':
         flag = False
 
     return flag
 
 def go_shopping():
-
-    cart = dict();
-
+    cart = Cart()
     while True:
-        order = get_order()
-        if not process_order(order, cart):
+        if not process_order(get_order(), cart):
             break
- 
-    print(cart)
-    print('Finished')
+    print("Finished Shopping")
+
 
 go_shopping()
